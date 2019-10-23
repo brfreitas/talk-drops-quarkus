b9e39a51e0320036228895411851ac54c109175e
Bruno Freitas <brunofreitas7@gmail.com>
2019-10-22 21:51:09 -0300

    Create spring boot project

---
+++ shouldideploytoday/src/main/java/io/github/brfreitas/shouldideploytoday/ShouldideploytodayApplication.java
@@ -0,0 +1,13 @@
+package io.github.brfreitas.shouldideploytoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShouldideploytodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShouldideploytodayApplication.class, args);
+	}
+
+}
---
+++ shouldideploytoday/src/test/java/io/github/brfreitas/shouldideploytoday/ShouldideploytodayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.brfreitas.shouldideploytoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShouldideploytodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
